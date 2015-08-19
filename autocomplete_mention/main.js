be153f4325e580a21a9a431200be42b4612200b6
johnwquarles <john.w.quarles@gmail.com>
2015-08-19 18:13:19 -0500

    oh, it wants me to use strict?

--- autocomplete_mention/main.js
+++ autocomplete_mention/main.js
@@ -1,8 +1,9 @@
-/*jshint -W097 */
 /*global $:false */
 
 // needs jQuery, jQuery-ui, jQuery-ui smoothness CSS theme
 // (or we could make our own styling for the autocomplete menu and would just need the first two).
+
+"use strict";
 
 $(function() {
 
